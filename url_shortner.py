902ff048837e24bce018223cf46fdcd9da7196ff
Anubhav Patrick <anubhavpatrick@gmail.com>
2022-05-24 12:32:54 +0530

    Updated url-shortner

--- url_shortner.py
+++ url_shortner.py
@@ -2,58 +2,46 @@ from hashlib import sha256
 import pickle
 import os
 
-def shorten(url:str, shortened_url_dict:dict)-> str:
+def shorten(url:str, shortened_url_dict:dict):
     '''A function to hash the url to 6 digit hash. If the url is already in the dictionary, 
     return the next hash.
+
     Args:
-        url: the original long url to be shortned (hashed)
-        shortened_url_dict: a dictionary containing mapping of long urls to their short urls
+        url: the url to be hashed
     
     Returns:
-        the hashed shortened url in hexadecimal of length 6
+        the hashed shortened url
     '''
-    shortened_url = None
-    
-    #TODO: if url already in the dictionary, return the shortened url
-    
-    
-    #salt is to add randomization to the string before hashing
+    #if url already in the dictionary, return the shorted url
+    if url in shortened_url_dict.keys():
+        return shortened_url_dict[url]
+
+    #create new shortened url if the url is not in the dictionary
     salt = str(os.urandom(16))
+    shortened_url = sha256((url+salt).encode()).hexdigest()[:6]
+    while shortened_url in shortened_url_dict.values():
+        shortened_url = hex(int(shortened_url,16)+1)[:6]
     
-    #TODO: create new shortened url in hexadecimal if the url is not in the dictionary
-    #shortened_url = ???
-    
-    
-    #TODO: if the url is already in shortened_url_dict then add 1 to generated shortened url and continue rechecking till a shortened url is found
-    #that is not in shortened_url_dict
-    
-    
-    #TODO: save the shortened url in the dictionary
-    #shortened_url_dict[url] = ???
-    
-    
+    #save the shortened url in the dictionary
+    shortened_url_dict[url] = shortened_url
     return shortened_url
- 
+    
 
 def restore(shortened_url, shortened_url_dict):
-    '''A function to restore the long url from the short url
+    '''A function to restore the url from the short url
 
     Args:
         shortened_url: the short url
-        shortened_url_dict: a dictionary containing mapping of long urls to their short urls
     
     Returns:
-        the original long url
+        the original url
     '''
-    
-    # TODO: if the short url is not present in shortened_url_dict then return None
-    
+    if shortened_url not in shortened_url_dict.values():
+        return None
 
-    # TODO: if the short url is present in shortened_url_dict then return the corresponding long url
-    
-    
-    # TODO: Delete next line
-    pass
+    for k, v in shortened_url_dict.items():
+        if v == shortened_url:
+            return k
 
 
 #Do not change the main function
@@ -70,6 +58,8 @@ def main():
     print(shorten("www.google.com", shortened_url_dict))
     print(shorten("www.msn.com", shortened_url_dict))
     print(shorten("www.yahoo.com", shortened_url_dict))
+    #if the user enters same long url multiple times then same short url must be generated
+    print(shorten("www.googlemail.com", shortened_url_dict))
     print(shorten("www.googlemail.com", shortened_url_dict))
     print(restore("zLg6wl", shortened_url_dict)) #this should return None
     
